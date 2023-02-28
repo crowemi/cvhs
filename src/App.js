0d162e93f3c91617f4ba3423f9c8310b2b48b2a3
crowemi <crowemi@hotmail.com>
2023-02-28 06:06:20 -0800

    updated lots.. add service

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
-import ContactForm from './ContactForm';
+import JoinRegistry from './JoinRegistry';
 
 function App() {
   return (
     <div className="App">
-      <div class="container-fluid">
-        <div class="col-12">
-          <h1 class="display-3 text-center welcome-heading">Welcome, Class of 2004</h1>
-          <p class="lead welcome-copy">
+      <div className="container-fluid">
+        <div className="col-12">
+          <h1 className="display-3 text-center welcome-heading">Welcome, Class of 2004</h1>
+          <p className="lead welcome-copy">
             Thank you for visiting this website. The purpose of this website is to serve as a class registry that enables us to maintain contact with our fellow alumni. Previously, social media platforms fulfilled this function. However, due to the diversity of platforms and the possibility that some alumni have discontinued their social media usage, we deemed this registry necessary.
           </p>
         </div>
         <hr />
-        <div class="col-12">
-          <ContactForm />
+        <div className="col-12">
+          <JoinRegistry />
         </div>
       </div>
     </div>
---
+++ src/JoinRegistry.js
@@ -0,0 +1,85 @@
+import React from 'react'
+
+class JoinRegistry extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+            registryCount: 0
+        }
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:3001/metrics/")
+            .then(res => res.json()).then(res => { this.setState({ registryCount: res.registryCount }) })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const formElem = document.querySelector('form');
+        const formData = new FormData(formElem);
+
+        var form = {
+            firstName: formData.get("firstName"),
+            lastName: formData.get("lastName"),
+            email: formData.get("emailAddress")
+        }
+        console.log(JSON.stringify(form));
+        var options = {
+            method: "POST",
+            // mode: "cors",
+            body: JSON.stringify(form),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        }
+        fetch("http://localhost:3001/registry/", options).then(
+            (res) => {
+                console.log(res.json());
+                var current_state = this.state;
+                current_state.registryCount = this.state.registryCount + 1;
+                this.setState(current_state);
+            }
+        ).catch(() => { console.log("Error") })
+
+    }
+
+    render() {
+        return (
+            <div className="container registry-form-container" >
+                <div className="col-12">
+                    <div className="row">
+                        <div className="col-6">
+                            <h1 className="display-5">:join-registry</h1>
+                        </div>
+                        <div className="col-6">
+                            <div className="d-flex flex-column-reverse">
+                                <div className="p-2"></div>
+                                <div className="p-2"><div className="float-end"><span className="badge rounded-pill text-bg-primary">{this.state.registryCount}</span> classmates registered</div></div>
+                                <div className="p-2"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <form onSubmit={this.handleSubmit} className="row g-3">
+                    <div className="input-group mb-3">
+                        <input name="firstName" type="text" className="form-control" placeholder="First Name" />
+                    </div>
+                    <div className="input-group mb-3">
+                        <input name="lastName" type="text" className="form-control" placeholder="Last (Maiden) Name" />
+                    </div>
+                    <div className="input-group mb-3">
+                        <span className="input-group-text" id="basic-addon1">@</span>
+                        <input name="emailAddress" type="text" className="form-control" placeholder="Email Adress" />
+                    </div>
+                    <button className="btn btn-primary" type="submit">Submit</button>
+                </form >
+            </ div >
+        );
+    };
+}
+
+export default JoinRegistry
